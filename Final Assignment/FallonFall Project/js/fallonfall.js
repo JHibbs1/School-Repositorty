c77a464e2ee725a2000b49ecc40dad98664b84be
JHibbs1 <jesse.hibbs@hotmail.com>
2017-08-08 00:07:24 -0700

    Submitting changes to final project

--- Final Assignment/FallonFall Project/js/fallonfall.js
+++ Final Assignment/FallonFall Project/js/fallonfall.js
@@ -1,24 +1,13 @@
 
 $(document).ready(function(){
-	$(".aboutthesegentlemen").click(function(event){
-		event.preventDefault();
-		$(".aboutthesegentlemen").hide();
-		$("#show-click").slideDown("slow");
-		$(".aboutthesegentlemen").show();
-	});
-	$(".aboutthesegentlemen").click(function(event){
-		event.preventDefault();
-		$(".epicthanks").hide();
-		$("#aboutthesegentlemen").slideDown("slow");
-	});
-	$(".aboutthesegentlemen").click(function(event){
-		event.preventDefault();
-		$("#show-click").slideUp("slow");
-		$(".epicthanks").hide();
-		$(".aboutthesegentlemen").show();
-	});
-	//if about these gentlemen is clicked
-	//show paragraph
-	//if epic thanks is clicked
-	//hide paragraph
-	
+  console.log("loaded");
+  $(".toggle").on("click", function(event){
+    event.preventDefault();
+    $(".aboutthesegentlemen").slideToggle();
+    var oldText = $(this).text();
+   var newText = $(this).data("toggleText");
+   $(this).text(newText);
+   $(this).data("toggleText",oldText);
+  })
+
+});
